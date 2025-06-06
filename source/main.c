3dfa37b3b72c778f6cf47fa1a6ef403c5918d9c7
AssoDePicche <samuelprado730@gmail.com>
2025-06-06 19:29:08 -0300

    refactor: error message formatting

--- source/main.c
+++ source/main.c
@@ -4,7 +4,7 @@
 
 int main(const int argc, const char **argv) {
   if (2 != argc) {
-    fprintf(stderr, "You must provide only the grammar file\n");
+    fprintf(stderr, "Error: You must provide only the grammar file\n");
 
     return 1;
   }
@@ -12,7 +12,8 @@ int main(const int argc, const char **argv) {
   FILE *stream = fopen(argv[1], "r");
 
   if (NULL == stream) {
-    fprintf(stderr, "The grammar file '%s' could not be read\n", argv[1]);
+    fprintf(stderr, "Error: The grammar file '%s' could not be read\n",
+            argv[1]);
 
     return 1;
   }
@@ -20,7 +21,7 @@ int main(const int argc, const char **argv) {
   printf("%s\n", ParseStream(stream) ? "Correct" : "Incorrect");
 
   if (EOF == fclose(stream)) {
-    fprintf(stderr, "I/O error when closing the '%s' stream\n", argv[1]);
+    fprintf(stderr, "Error: I/O error when closing the '%s' stream\n", argv[1]);
 
     return 1;
   }
